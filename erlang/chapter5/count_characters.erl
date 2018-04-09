b632a22adce65e89fa30bee5932b6ceda277eea6
dongshijun <dongshijun2010@126.com>
2018-04-10 04:14:05 +0800

    20180409日常代码

--- erlang/chapter5/count_characters.erl
+++ erlang/chapter5/count_characters.erl
@@ -4,9 +4,13 @@
 count_characters(Str)	->
 	count_characters(Str,#{}).
 
-count_characters([Head|T], #{ Head := N }=X)->
-	count_characters(T, X#{ Head := N+1 });
-count_characters([Head|T], X)->
-	count_characters(T, X#{ Head => 1 });
+%% count_characters([H|T], #{ H := N }=X)->
+%%	count_characters(T, X#{ H := N+1 });
+%% count_characters([H|T], X)->
+%%	count_characters(T, X#{ H => 1 });
+
+count_characters([H|T], X) -> 
+ count_characters(T, maps:put(H, maps:get(H, X, 0) + 1, X));
+%% maps:get(H,X,0),如果没有则赋值为0，在使用maps:put,改变映射的关系中的值,maps:put(H,1,X)
 count_characters([], X)	->
 	X.
